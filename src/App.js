4913064415cf9e2b9dab90fe6baccbb2c9456c99
Moin Ahmed <ahmedmoin234@gmail.com>
2023-07-03 18:25:31 +0200

    learning about fragments, portals and refs

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Fragment, useState } from "react";
+import AddUser from "./Components/Users/AddUser";
+import UserList from "./Components/Users/UserList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
+    </Fragment>
+  );
+}
+
+export default App;
